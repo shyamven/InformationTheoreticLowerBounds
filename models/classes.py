2b9e3d468d6a3c09c94fa91081a72ebbf5bcf297
Shyam Venkatasubramanian <shyam.venkatasubramanian@gmail.com>
2023-12-05 21:49:55 -0500

    Update classes.py

--- models/classes.py
+++ models/classes.py
@@ -1,1 +1,26 @@
+import torch
+import torch.nn as nn
 
+# Define the autoencoder
+class Autoencoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, latent_dim):
+        super(Autoencoder, self).__init__()
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.Sigmoid(),
+            nn.Linear(hidden_dim, latent_dim),
+            nn.Sigmoid()
+        )
+        # Decoder
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim, hidden_dim),
+            nn.Sigmoid(),
+            nn.Linear(hidden_dim, input_dim),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
