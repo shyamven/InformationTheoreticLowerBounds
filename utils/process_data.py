b46f6c43415e5f0767437aa1f57b8dffe36c1a4a
Shyam Venkatasubramanian <shyam.venkatasubramanian@gmail.com>
2023-12-10 05:29:26 -0500

    Add files via upload

--- utils/process_data.py
+++ utils/process_data.py
@@ -1,27 +1,45 @@
 import numpy as np
 from scipy import optimize
-from scipy.stats import truncnorm
-import torch
+
 
 def FindMSELowerBound(d, l, h, K):
-    start = 10**-15
-    while f(start, d, l, h, K) < 0:
-        start /= 10
-    end = 10**2
-    while f(end, d, l, h, K) > 0:
-        end *= 10
-    assert f(start, d, l, h, K) > 0.0
-    assert f(end, d, l, h, K) < 0.0
+    # Initialization
+    x = 1e-60
+    original_step = 1e-60
     
-    # Use lambda to create a new function that only varies x
-    f_p_optimized = lambda x: f_p(x, d, l, h, K)
+    # Initializing loop
+    step = original_step
+    add = True
+    num_steps_between = 0
+    # directional_changes = 0
     
-    root = optimize.bisect(f_p_optimized, start, end)
-    assert root > 0
-    maximum = f(root, d, l, h, K)
-    return maximum
+    # Iterative process
+    while True:
+        current_f = f(x, d, l, h, K)
+        if add == True: x += step
+        else: x -= step
+        
+        next_f = f(x, d, l, h, K)
+        step *= 1.01
     
+        num_steps_between += 1
     
+        if (next_f - current_f) < 0:
+            break
+        
+            # print(f"Direction change #{directional_changes} at x = {x}, f(x) = {current_f}")
+            # print(num_steps_between)
+            # if num_steps_between <= 30: break
+            # else: num_steps_between = 0
+    
+            # step = original_step
+            # directional_changes += 1
+            # if directional_changes % 2 == 1: add = False
+            # else: add = True
+            
+    return current_f
+
+
 def f(x, d, l, h, K):
     power = (d+l-1)/d
     term1 = (d/ ((2*np.pi*np.e)**power)) * np.exp(2*h/d)
@@ -31,20 +49,28 @@ def f(x, d, l, h, K):
     return x * (term1*term2*term3 - K*l)
 
 
-def f_p(x, d, l, h, K):
-    power = (d+l-1)/d
-    A = (d/ ((2*np.pi*np.e)**power)) * np.exp(2*h/d)
-    B = K*l
-    num = ((x+0.25)**(-1 - l/d)) * (-A*l*x**(1+l/d) + A*x**(1/d)*(x + 0.25) - B*d*x*(x+0.25)**(1+l/d))
-    return num/(d*x)
+# def f_p(x, d, l, h, K):
+#     power = (d+l-1)/d
+#     A = (d/ ((2*np.pi*np.e)**power)) * np.exp(2*h/d)
+#     B = K*l
+#     num = ((x+0.25)**(-1 - l/d)) * (-A*l*x**(1+l/d) + A*x**(1/d)*(x + 0.25) - B*d*x*(x+0.25)**(1+l/d))
+#     return num/(d*x)
 
 
-def generate_truncated_normal_data(train_size, input_dim, lower_bound=0, upper_bound=1, mu=0, sigma=1):
-    # Standardizing the bounds for truncation
-    a, b = (lower_bound - mu) / sigma, (upper_bound - mu) / sigma
-
-    # Generating truncated normal data
-    data = truncnorm.rvs(a, b, loc=mu, scale=sigma, size=(train_size, input_dim))
-
-    # Converting to PyTorch tensor
-    return torch.tensor(data, dtype=torch.float32)
+# def FindMSELowerBound(d, l, h, K):
+#     start = 10**-15
+#     while f(start, d, l, h, K) < 0:
+#         start /= 10
+#     end = 10**2
+#     while f(end, d, l, h, K) > 0:
+#         end *= 10
+#     assert f(start, d, l, h, K) > 0.0
+#     assert f(end, d, l, h, K) < 0.0
+    
+#     # Use lambda to create a new function that only varies x
+#     f_p_optimized = lambda x: f_p(x, d, l, h, K)
+    
+#     root = optimize.bisect(f_p_optimized, start, end)
+#     assert root > 0
+#     maximum = f(root, d, l, h, K)
+#     return maximum
--- utils/upload_data.py
+++ utils/upload_data.py
@@ -1,5 +1,6 @@
 import torch
-from utils.process_data import generate_truncated_normal_data
+from scipy.stats import truncnorm
+
 
 def LoadDataset(DatasetName, train_size, input_dim):
     if DatasetName == "Uniform":
@@ -10,11 +11,20 @@ def LoadDataset(DatasetName, train_size, input_dim):
         X_test = torch.rand(train_size, input_dim, generator=gen0)
         output1, output2 = X_train, X_test
         
-    elif DatasetName == "TruncatedNormal":     
+    elif DatasetName == "TruncatedNormal":
+        # Use same dataset across different trials for reproducibility
         gen0 = torch.Generator()
         gen0.manual_seed(0)
         X_train = generate_truncated_normal_data(train_size, input_dim)
         X_test = generate_truncated_normal_data(train_size, input_dim)
+        output1, output2 = X_train, X_test
+        
+    elif DatasetName == 'ScaledUniform':
+        # Use same dataset across different trials for reproducibility
+        gen0 = torch.Generator()
+        gen0 = gen0.manual_seed(0)
+        X_train = torch.rand(train_size, input_dim, generator=gen0) * 0.5
+        X_test = torch.rand(train_size, input_dim, generator=gen0) * 0.5
         output1, output2 = X_train, X_test
         
     else:
@@ -22,3 +32,14 @@ def LoadDataset(DatasetName, train_size, input_dim):
         exit(1)
   
     return output1, output2
+
+
+def generate_truncated_normal_data(train_size, input_dim, lower_bound=0, upper_bound=1, mu=0, sigma=1):
+    # Standardizing the bounds for truncation
+    a, b = (lower_bound - mu) / sigma, (upper_bound - mu) / sigma
+
+    # Generating truncated normal data
+    data = truncnorm.rvs(a, b, loc=mu, scale=sigma, size=(train_size, input_dim))
+
+    # Converting to PyTorch tensor
+    return torch.tensor(data, dtype=torch.float32)
