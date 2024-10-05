0355ef654da90c1df22244173b2ea50c2a75c873
Shyam Venkatasubramanian <shyam.venkatasubramanian@gmail.com>
2024-10-05 01:06:09 -0400

    Add files via upload

--- utils/process_data.py
+++ utils/process_data.py
@@ -1,76 +1,51 @@
 import numpy as np
 from scipy import optimize
 
-
-def FindMSELowerBound(d, l, h, K):
-    # Initialization
-    x = 1e-60
-    original_step = 1e-60
+def ApproxMSELowerBound(d, l, h_x, K):
+    # Calculating approximate s*
+    num = 0.25 * 2**(4*l/d) * l * np.exp(-2 + 4*h_x/d)
+    den = np.pi**2 * K**2 * d
+    expon = d / (d - 2*l)
+    s_hat = (num / den) ** expon
     
-    # Initializing loop
-    step = original_step
-    add = True
-    num_steps_between = 0
-    # directional_changes = 0
+    # Substitute s* approximation into MSE LB Function
+    LB = (d/(2*np.pi*np.e)) * np.exp(2*h_x/d) * (s_hat / (0.25 + s_hat))**(l/d)  - 2*K*np.sqrt(d*l*s_hat)
     
-    # Iterative process
-    while True:
-        current_f = f(x, d, l, h, K)
-        if add == True: x += step
-        else: x -= step
-        
-        next_f = f(x, d, l, h, K)
-        step *= 1.01
-    
-        num_steps_between += 1
-    
-        if (next_f - current_f) < 0:
-            break
-        
-            # print(f"Direction change #{directional_changes} at x = {x}, f(x) = {current_f}")
-            # print(num_steps_between)
-            # if num_steps_between <= 30: break
-            # else: num_steps_between = 0
-    
-            # step = original_step
-            # directional_changes += 1
-            # if directional_changes % 2 == 1: add = False
-            # else: add = True
-            
-    return next_f
-
-
-def f(x, d, l, h, K):
-    power = (d+l-1)/d
-    term1 = (d/ ((2*np.pi*np.e)**power)) * np.exp(2*h/d)
-    term2 = (0.25 + x)**(-l/d)
-    term3 = x**((1-d)/d)
-
-    return x * (term1*term2*term3 - K*l)
-
-
-# def f_p(x, d, l, h, K):
-#     power = (d+l-1)/d
-#     A = (d/ ((2*np.pi*np.e)**power)) * np.exp(2*h/d)
-#     B = K*l
-#     num = ((x+0.25)**(-1 - l/d)) * (-A*l*x**(1+l/d) + A*x**(1/d)*(x + 0.25) - B*d*x*(x+0.25)**(1+l/d))
-#     return num/(d*x)
-
+    return LB
 
 # def FindMSELowerBound(d, l, h, K):
-#     start = 10**-15
-#     while f(start, d, l, h, K) < 0:
-#         start /= 10
-#     end = 10**2
-#     while f(end, d, l, h, K) > 0:
-#         end *= 10
-#     assert f(start, d, l, h, K) > 0.0
-#     assert f(end, d, l, h, K) < 0.0
+#     # Initialization
+#     x = 1e-100
+#     original_step = 1e-100
     
-#     # Use lambda to create a new function that only varies x
-#     f_p_optimized = lambda x: f_p(x, d, l, h, K)
+#     # Initializing loop
+#     step = original_step
+#     add = True
+#     num_steps_between = 0
+#     # directional_changes = 0
     
-#     root = optimize.bisect(f_p_optimized, start, end)
-#     assert root > 0
-#     maximum = f(root, d, l, h, K)
-#     return maximum
+#     # Iterative process
+#     while True:
+#         current_f = f(x, d, l, h, K)
+#         if add == True: x += step
+#         else: x -= step
+        
+#         next_f = f(x, d, l, h, K)
+#         step *= 1.01
+    
+#         num_steps_between += 1
+    
+#         if (next_f - current_f) < 0:
+#             break
+        
+#             # print(f"Direction change #{directional_changes} at x = {x}, f(x) = {current_f}")
+#             # print(num_steps_between)
+#             # if num_steps_between <= 30: break
+#             # else: num_steps_between = 0
+    
+#             # step = original_step
+#             # directional_changes += 1
+#             # if directional_changes % 2 == 1: add = False
+#             # else: add = True
+            
+#     return next_f
--- utils/upload_data.py
+++ utils/upload_data.py
@@ -1,5 +1,6 @@
 import torch
-from scipy.stats import truncnorm
+from scipy.stats import truncnorm, norm, beta as beta_dist
+import numpy as np
 
 
 def LoadDataset(DatasetName, train_size, input_dim):
@@ -8,30 +9,84 @@ def LoadDataset(DatasetName, train_size, input_dim):
         gen0 = torch.Generator()
         gen0 = gen0.manual_seed(0)
         X_train = torch.rand(train_size, input_dim, generator=gen0)
-        X_test = torch.rand(train_size, input_dim, generator=gen0)
+        X_test = torch.rand(10000, input_dim, generator=gen0)
         output1, output2 = X_train, X_test
+        
+        h_x = np.log(1) * input_dim # Entropy for d (i.i.d) RVs: h(X) = np.log(1 - 0) * d
+        
+    elif DatasetName == 'Beta':
+        # Beta distribution parameters
+        alpha_beta_dist = 100
+        beta_beta_dist = 1
+        
+        # Generate data from the Beta distribution using NumPy
+        np.random.seed(0) # For reproducibility
+        X_train_np = np.random.beta(alpha_beta_dist, beta_beta_dist, (train_size, input_dim))
+        X_test_np = np.random.beta(alpha_beta_dist, beta_beta_dist, (10000, input_dim))
+        
+        # Convert NumPy arrays to PyTorch tensors
+        X_train = torch.tensor(X_train_np, dtype=torch.float32)
+        X_test = torch.tensor(X_test_np, dtype=torch.float32)
+        output1, output2 = X_train, X_test
+        
+        h_x = beta_dist.entropy(alpha_beta_dist, beta_beta_dist) * input_dim # Entropy for d (i.i.d) RVs
+        print(h_x)
         
     elif DatasetName == "TruncatedNormal":
         # Use same dataset across different trials for reproducibility
         gen0 = torch.Generator()
         gen0.manual_seed(0)
         X_train = generate_truncated_normal_data(train_size, input_dim)
-        X_test = generate_truncated_normal_data(train_size, input_dim)
+        X_test = generate_truncated_normal_data(10000, input_dim)
         output1, output2 = X_train, X_test
+        
+        # Parameters for the standard normal distribution truncated to [0,1]
+        mu, sigma, a, b = 0, 1, 0, 1
+        alpha = (a - mu) / sigma # Standardized lower bound
+        beta = (b - mu) / sigma # Standardized upper bound
+        Z = norm.cdf(beta) - norm.cdf(alpha) # Normalization constant Z
+        
+        # Entropy calculation using the provided formula
+        entropy_trunc_normal = np.log(np.sqrt(2 * np.pi * np.e) * sigma * Z) + ((alpha * norm.pdf(alpha) - beta * norm.pdf(beta)) / (2 * Z))
+        
+        h_x = entropy_trunc_normal * input_dim # Entropy for d (i.i.d) RVs
         
     elif DatasetName == 'ScaledUniform':
         # Use same dataset across different trials for reproducibility
         gen0 = torch.Generator()
         gen0 = gen0.manual_seed(0)
         X_train = torch.rand(train_size, input_dim, generator=gen0) * 0.5
-        X_test = torch.rand(train_size, input_dim, generator=gen0) * 0.5
+        X_test = torch.rand(10000, input_dim, generator=gen0) * 0.5
         output1, output2 = X_train, X_test
+        
+        h_x = np.log(0.5) * input_dim # Entropy for d (i.i.d) RVs: h(X) = np.log(0.5 - 0) * d
         
     else:
         print("Error: Dataset name is undefined")
         exit(1)
   
-    return output1, output2
+    return output1, output2, h_x
+
+
+def LoadDatasetReg(DatasetName, train_size, input_dim, output_dim):
+    if DatasetName == "Uniform":
+        # Use same dataset across different trials for reproducibility
+        gen0 = torch.Generator()
+        gen0 = gen0.manual_seed(0)
+        X_train = torch.rand(train_size, input_dim, generator=gen0)
+        X_test = torch.rand(10000, input_dim, generator=gen0)
+        
+        # Define the transformation weights and bias for y
+        weights = torch.rand(input_dim, output_dim, generator=gen0)
+        bias = torch.rand(output_dim, generator=gen0)
+        
+        # Generate y_train and y_test as a linear transformation of X with added noise
+        y_train = X_train @ weights + bias + 0.1 * torch.randn(train_size, output_dim, generator=gen0)
+        y_test = X_test @ weights + bias + 0.1 * torch.randn(10000, output_dim, generator=gen0)
+        
+        h_x = np.log(1) * input_dim # Entropy for d (i.i.d) RVs: h(X) = np.log(1 - 0) * d
+        
+        return X_train, y_train, X_test, y_test, h_x
 
 
 def generate_truncated_normal_data(train_size, input_dim, lower_bound=0, upper_bound=1, mu=0, sigma=1):
